afba40ba0abc54f1fd6f2a582c0927d156fdff6f
matgabo <wisdi.gabo44@gmail.com>
2023-07-30 23:05:35 +0800

    add consumer program

---
+++ src/PlaylistsService.js
@@ -0,0 +1,43 @@
+const { Pool } = require('pg');
+
+class PlaylistsService {
+  constructor() {
+    this._pool = new Pool();
+  }
+
+  async getPlaylists(userId) {
+    const query = {
+      text: `
+      SELECT playlists.id, playlists.name FROM playlists
+      LEFT JOIN collaborations ON collaborations.playlist_id = playlists.id
+      WHERE playlists.owner = $1 OR collaborations.user_id = $1`,
+      values: [userId],
+    };
+
+    const result = await this._pool.query(query);
+
+    return result.rows[0];
+  }
+
+  async getSongsInPlaylist(playlistId) {
+    const query = {
+      text: `
+      SELECT 
+        songs.id,
+        songs.title,
+        songs.performer
+      FROM
+        songs
+      LEFT JOIN
+        playlist_songs ON songs.id = playlist_songs.song_id
+      WHERE
+        playlist_songs.playlist_id = $1`,
+      values: [playlistId],
+    };
+
+    const result = await this._pool.query(query);
+    return result.rows;
+  }
+}
+
+module.exports = PlaylistsService;
--- src/validator/exports/schema.js
+++
@@ -1,7 +0,0 @@
-const Joi = require('joi');
-
-const ExportsPlaylistPayloadSchema = Joi.object({
-  targetEmail: Joi.string().email({ tlds: true }).required(),
-});
-
-module.exports = ExportsPlaylistPayloadSchema;
